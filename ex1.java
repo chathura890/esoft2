668944b6ef919483ffa64567c7e887a85adf90d2
chathura <chathura555maduranga555@gmail.com>
2024-10-03 14:20:55 +0530

    Corrected commit message

--- ex1.java
+++ ex1.java
@@ -5,3 +5,18 @@ public class ex1 {
         System.out.println("Goodbye world!");
     }
 }
+public class ex1 {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("It's been nice knowing you");
+        System.out.println("Goodbye world!");
+    }
+}
+public class ex1 {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("It's been nice knowing you");
+        System.out.println("Goodbye world!");
+    }
+}
+
