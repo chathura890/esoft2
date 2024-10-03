5e84e22ebb70ec635171453cf8bf57e48f69d793
chathura <chathura555maduranga555@gmail.com>
2024-10-03 14:32:07 +0530

    added files

---
+++ ex2.java
@@ -0,0 +1,16 @@
+public class ex2{
+    public static void main(String[] args) {
+
+        System.out.println("=============================================");
+        System.out.println("="+"\t\t"+"Student information"+"\t"+"=");
+        System.out.println("=============================================");
+        System.out.println("=Name"+"\t\t"+":ARDP Ranasignhe"+"=");
+        System.out.println("=Reg No"+"\t\t"+":DIS_08_M4_1234"+"\t"+"=");
+        System.out.println("=Address"+"\t"+": Malabe"+"\t"+"=");
+        System.out.println("=============================================");
+
+
+
+
+    }
+}
---
+++ ex3.java
@@ -0,0 +1,17 @@
+public class ex3{
+    public static void main(String[] args) {
+     
+    int age=4;
+    if(age>18){
+       System.out.println("Adult");
+       
+    }
+
+    else {
+       
+       System.out.println("Child");
+
+
+        }
+    }
+}
